c006a34c40b8a26054b365db0db34378bd145698
Carlos Pereira Atencio <carlos@microbit.org>
2024-06-06 19:38:55 +0100

    Add test files & empty shims and enums in hopes MakeCode regenerates on build.

--- enums.d.ts
+++ enums.d.ts
@@ -1,21 +1,3 @@
 // Auto-generated. Do not edit.
 
-
-    declare const enum MlRunnerIds {
-    MlRunnerInference = 71,
-    MlRunnerTimer = 72,
-    }
-
-
-    declare const enum MlRunnerError {
-    ErrorModelNotPresent = 800,
-    ErrorSamplesLength = 801,
-    ErrorSamplesDimension = 802,
-    ErrorSamplesPeriod = 803,
-    ErrorInputLength = 804,
-    ErrorMemAlloc = 805,
-    ErrorModelInference = 806,
-    ErrorDataProcessing = 807,
-    }
-
 // Auto-generated. Do not edit. Really.
--- shims.d.ts
+++ shims.d.ts
@@ -1,9 +1,3 @@
 // Auto-generated. Do not edit.
-declare namespace mlrunner {
-
-    /*************************************************************************/
-    //% blockId=mlrunner_init shim=mlrunner::init
-    function init(model_str: Buffer): void;
-}
 
 // Auto-generated. Do not edit. Really.
